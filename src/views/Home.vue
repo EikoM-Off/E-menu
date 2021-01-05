23cf03476f011c92b2b3f77aeaaba0ae94648bac
SuperBad-Off <zar.jar@mail.ru>
2021-01-05 16:43:54 +0600

    Dishes List created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,79 @@
 <template>
 <div>
   <div>
-    <h1>Основная страница</h1>
+    <h1>{{'pop_dish' | localize}}</h1>
+
+    <div class="row">
+    <div class="col s12">
+
+    <div class="col s12 m6 l3">
+       <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://im0-tub-kz.yandex.net/i?id=6274e99ca195c9813549526fe1a6faf1-l&n=13">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Стейк из говядины<i class="material-icons right">more_vert</i></span>
+      <p>Очень вкусное и сочное мясо на гриле.<br>
+        <br><a href="#">{{'add_to_order' | localize}}</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Ингредиенты:<i class="material-icons right">close</i></span>
+      <p>Говядина, Соевый соус, Масло кунжутное, Сахар коричневый, Имбирь, Чеснок, Перец чили, Соль, Розмарин</p>
+    </div>
+  </div>
+    </div>
+    <div class="col s12 m6 l3">
+       <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://im0-tub-kz.yandex.net/i?id=6274e99ca195c9813549526fe1a6faf1-l&n=13">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Стейк из говядины<i class="material-icons right">more_vert</i></span>
+      <p>Очень вкусное и сочное мясо на гриле.<br>
+        <br><a href="#">{{'add_to_order' | localize}}</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Ингредиенты:<i class="material-icons right">close</i></span>
+      <p>Говядина, Соевый соус, Масло кунжутное, Сахар коричневый, Имбирь, Чеснок, Перец чили, Соль, Розмарин</p>
+    </div>
+  </div>
+    </div>
+    <div class="col s12 m6 l3">
+       <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://im0-tub-kz.yandex.net/i?id=6274e99ca195c9813549526fe1a6faf1-l&n=13">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Стейк из говядины<i class="material-icons right">more_vert</i></span>
+      <p>Очень вкусное и сочное мясо на гриле.<br>
+        <br><a href="#">{{'add_to_order' | localize}}</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Ингредиенты:<i class="material-icons right">close</i></span>
+      <p>Говядина, Соевый соус, Масло кунжутное, Сахар коричневый, Имбирь, Чеснок, Перец чили, Соль, Розмарин</p>
+    </div>
+  </div>
+    </div>
+    <div class="col s12 m6 l3">
+       <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://im0-tub-kz.yandex.net/i?id=6274e99ca195c9813549526fe1a6faf1-l&n=13">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Стейк из говядины<i class="material-icons right">more_vert</i></span>
+      <p>Очень вкусное и сочное мясо на гриле.<br>
+        <br><a href="#">{{'add_to_order' | localize}}</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Ингредиенты:<i class="material-icons right">close</i></span>
+      <p>Говядина, Соевый соус, Масло кунжутное, Сахар коричневый, Имбирь, Чеснок, Перец чили, Соль, Розмарин</p>
+    </div>
+  </div>
+    </div>
+
+    </div>
+  </div>
+
   </div>
 
  <FloatButton/>
---
+++ src/views/OrderPage.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <div>
+    <h1>{{'order' | localize}}</h1>
+
+  </div>
+
+</div>
+</template>
+
+<script>
+// import preloader from '@/components/PreLoader'
+// import M from 'materialize-css'
+export default {
+  components: {
+  }
+}
+</script>
