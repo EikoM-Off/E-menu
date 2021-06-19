db8792c48876126e4bb801d9b2e51ed6285c8f61
SuperBad-Off <66469975+SuperBad-Off@users.noreply.github.com>
2021-06-19 18:21:19 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,16 +16,7 @@ Vue.use(messagePlugin)
 Vue.filter('localize', localizeFilter)
 Vue.use(Vuelidate)
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyBilDwYRM23fAvjZlmr4hTZgcFVQl9v89A',
-  authDomain: 'e-menu-57517.firebaseapp.com',
-  databaseURL: 'https://e-menu-57517-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'e-menu-57517',
-  storageBucket: 'e-menu-57517.appspot.com',
-  messagingSenderId: '162702185456',
-  appId: '1:162702185456:web:e2be8f9b74622d08e7c335',
-  measurementId: 'G-DHZQ3JZNVF'
-}
+//config here
 
 firebase.initializeApp(firebaseConfig)
 
